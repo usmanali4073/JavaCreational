55f12c4d773e238fcff7b02edf501d79ecd02af7
Usman <usmanali4073@gmail.com>
2019-10-15 22:15:07 -0400

    First Commit

---
+++ src/main/java/com/speaya/creational/CreationalApplication.java
@@ -0,0 +1,13 @@
+package com.speaya.creational;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreationalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CreationalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/speaya/creational/CreationalApplicationTests.java
@@ -0,0 +1,16 @@
+package com.speaya.creational;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CreationalApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
